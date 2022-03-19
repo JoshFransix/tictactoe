1f963a29b870f180e18fc5b3db2423fb6a12c2ca
joshfransix <joshiandersonk69@gmail.com>
2022-03-19 17:50:27 +0100

    setFunctions

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const xClass = "x";
 const circleClass = "circle";
 
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
 let circleTurn;
 
 cellElements.forEach((cell) => {
@@ -9,8 +10,23 @@ cellElements.forEach((cell) => {
 });
 
 function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? circleClass : xClass;
   // Place mark
+  placeMark(cell, currentClass);
   // Check for win
   // Check for draw
   // Switch Turns
+  swapTurns();
+  setBoardHoverClass();
 }
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {}
