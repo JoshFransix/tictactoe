437ccce9078f1f0b1b3a18075fbfc3ccac124f87
joshfransix <joshiandersonk69@gmail.com>
2022-03-19 17:26:59 +0100

    createdConstVariables and set parameters for click functions

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const cellElements = document.querySelectorAll("[data-cell]");
+const xClass = "x";
+const circleClass = "circle";
 
-cellElements.forEach((cell) => {});
+const cellElements = document.querySelectorAll("[data-cell]");
+let circleTurn;
+
+cellElements.forEach((cell) => {
+  cell.addEventListener("click", handleClick, { once: true });
+});
+
+function handleClick(e) {
+  // Place mark
+  // Check for win
+  // Check for draw
+  // Switch Turns
+}
